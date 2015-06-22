ce3181e57da4e447d9953de31ae3e1353d1b54db
Matthew Rankin <matthew@cumulusware.com>
2015-06-22 10:36:25 -0500

    Added log level enum

--- context.go
+++ context.go
@@ -13,6 +13,29 @@ package libusb
 import "C"
 import "fmt"
 
+type logLevel int
+
+// Log level enumeration
+const (
+	LogLevelNone    logLevel = C.LIBUSB_LOG_LEVEL_NONE
+	LogLevelError   logLevel = C.LIBUSB_LOG_LEVEL_ERROR
+	LogLevelWarning logLevel = C.LIBUSB_LOG_LEVEL_WARNING
+	LogLevelInfo    logLevel = C.LIBUSB_LOG_LEVEL_INFO
+	LogLevelDebug   logLevel = C.LIBUSB_LOG_LEVEL_DEBUG
+)
+
+var logLevels = map[logLevel]string{
+	LogLevelNone:    "No messages ever printed by the library (default)",
+	LogLevelError:   "Error messages are printed to stderr",
+	LogLevelWarning: "Warning and error messages are printed to stderr",
+	LogLevelInfo:    "Informational messages are printed to stdout, warning and error messages are printed to stderr",
+	LogLevelDebug:   "Debug and informational messages are printed to stdout, warnings and errors to stderr",
+}
+
+func (level logLevel) String() string {
+	return logLevels[level]
+}
+
 // Context represents a libusb session/context.
 type context struct {
 	context *C.libusb_context
@@ -38,7 +61,7 @@ func (ctx *context) Exit() error {
 }
 
 // SetDebug sets the log message verbosity.
-func (ctx *context) SetDebug(level int) error {
+func (ctx *context) SetDebug(level logLevel) error {
 	C.libusb_set_debug(ctx.context, C.int(level))
 	return nil
 }
