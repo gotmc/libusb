41c75ab2281112be9b427a9d5b37f8ee364d48e1
Matthew Rankin <matthew@cumulusware.com>
2017-06-16 09:16:24 -0500

    Update copyright date

--- endpoint.go
+++ endpoint.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-2016 The libusb developers. All rights reserved.
+// Copyright (c) 2015-2017 The libusb developers. All rights reserved.
 // Project site: https://github.com/gotmc/libusb
 // Use of this source code is governed by a MIT-style license that
 // can be found in the LICENSE.txt file for the project.
