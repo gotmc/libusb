fa52f727473a7d77a6c74791afdee8bb729d7b2f
Matthew Rankin <matthew@cumulusware.com>
2022-05-06 06:52:32 -0500

    Update copyright for 2022

--- hotplug.go
+++ hotplug.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2015-2022 The libusb developers. All rights reserved.
+// Project site: https://github.com/gotmc/libusb
+// Use of this source code is governed by a MIT-style license that
+// can be found in the LICENSE.txt file for the project.
+
 package libusb
 
 // #cgo pkg-config: libusb-1.0
